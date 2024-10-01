cce8c54bc0b86b16645baf7336250ba340335c7e
zion <zion@DESKTOP-HJ8OSMC.>
2024-10-02 00:22:02 +0300

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
+// Title case fungit add ction
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => {
+    const splittedArr = tutorial.split(" ");
+    const capitalized = splittedArr.map(
+      (str) => str.charAt(0).toUpperCase() + str.slice(1)
+    );
+    return capitalized.join(" ");
+  });
+};
+
+// Example usage
+console.log(titleCased());
+ 
